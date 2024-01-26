088de7c7b559a10b39d35380e85f0622ee2a148c
baptrt <baptiste.taret@ens-rennes.fr>
2024-01-26 09:28:26 +0100

    Corrigé

--- Exercice1nonrécursif.c
+++ Exercice1nonrécursif.c
@@ -1,4 +1,5 @@
 #include <stdio.h> 
+#include <stdlib.h>
 
 int* tableau_initial_pascal(){
     int* tableau = malloc(sizeof(int) * 3);
@@ -8,7 +9,7 @@ int* tableau_initial_pascal(){
     return tableau;
 }
 
-int* tableau_nouvelle_ligne(int* ligne, int n){
+int* tableau_nouvelle_ligne_pascal(int* ligne, int n){
     int* nouvelle_ligne = malloc(sizeof(int) * (n+1));
     nouvelle_ligne[0] = 0;
     nouvelle_ligne[n] = 0;
@@ -27,6 +28,13 @@ void affiche_pascal(int n){
             printf("%i", ligne_actuelle[colonne + 1]);
         }
         printf("\n");
+        ligne_precedente = ligne_actuelle;
         ligne_actuelle = tableau_nouvelle_ligne_pascal(ligne_actuelle, ligne + 3);
+        free (ligne_precedente);
     }
+    free (ligne_actuelle);
+}
+
+int main(){
+    affiche_pascal(100);
 }
